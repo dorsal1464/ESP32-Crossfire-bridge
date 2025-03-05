650e3b1dd6a6a4077fc74ed653a2894b2eec95f4
Dor <dor@amd.com>
2025-03-06 00:07:51 +0200

    add AlfredoCRSF library with CRC8 implementation and example sketches

--- include/defines.h
+++ include/defines.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #define BAUD_RATE (115200)
-#define TX_ADDR {0x54, 0x43, 0xb2, 0xa9, 0x9f, 0x58}
-#define RX_ADDR {0x34, 0xb7, 0xda, 0xf8, 0xc2, 0x2c}
-#define ENC_KEY {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+#define TX_ADDR \
+    { 0x54, 0x43, 0xb2, 0xa9, 0x9f, 0x58 }
+#define RX_ADDR \
+    { 0x34, 0xb7, 0xda, 0xf8, 0xc2, 0x2c }
+#define ENC_KEY \
+    { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }
 
-typedef struct
-{
+typedef struct {
     unsigned frame_ctrl : 16;
     unsigned duration_id : 16;
     uint8_t addr1[6]; /* receiver address */
@@ -16,8 +18,18 @@ typedef struct
     uint8_t addr4[6]; /* optional */
 } wifi_ieee80211_mac_hdr_t;
 
-typedef struct
-{
+typedef struct {
     wifi_ieee80211_mac_hdr_t hdr;
     uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
 } wifi_ieee80211_packet_t;
+
+typedef struct PACKED {
+    crsf_sensor_gps_t gps;
+    crsf_sensor_vario_t vario;
+    crsf_sensor_baro_altitude_t baro;
+    crsf_sensor_battery_t battery;
+} telemetry_t;
+
+typedef struct PACKED {
+    crsf_channels_t channels;
+} rc_data_t;
--- src/rx_main.cpp
+++ src/rx_main.cpp
@@ -1,3 +1,4 @@
+#include <Adafruit_NeoPixel.h>
 #include <AlfredoCRSF.h>
 #include <Arduino.h>
 #include <WiFi.h>
@@ -13,6 +14,18 @@
 HardwareSerial crsfHwSerial(1);
 AlfredoCRSF crsf;
 static unsigned long g_led_on = 0;
+static unsigned long g_last_stats = 0;
+static const uint8_t peer_addr[] = TX_ADDR;
+static Adafruit_NeoPixel led_pixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+static void turn_off_led() {
+#if defined(SIMPLE_LED)
+    digitalWrite(LED_PIN, HIGH);
+#else
+    led_pixel.setPixelColor(0, 0, 0, 0);
+    led_pixel.show();
+#endif
+}
 
 static void setup_crsf_uart() {
     crsfHwSerial.begin(CRSF_BAUDRATE, SERIAL_8N1, PIN_RX, PIN_TX);
@@ -25,25 +38,53 @@ static void setup_crsf_uart() {
 }
 
 void rx_setup() {
+#if defined(SIMPLE_LED)
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+#else
+    led_pixel.begin();
+#endif
     Serial.println("rx_setup!");
     setup_crsf_uart();
 }
 
 void rx_loop() {
     crsf.update();
-    if (millis() - g_led_on > 50) {
-        digitalWrite(LED_PIN, HIGH);
+
+    if (millis() - g_led_on > 1000 / 20) {
+        turn_off_led();
         g_led_on = millis();
     }
-    crsf_channels_t pkt = {0};
-    crsf.queuePacket(CRSF_ADDRESS_CRSF_TRANSMITTER, CRSF_FRAMETYPE_RC_CHANNELS_PACKED, &pkt, sizeof(pkt));
+
+    // crsf_channels_t pkt = {0};
+    // crsf.queuePacket(CRSF_ADDRESS_CRSF_TRANSMITTER, CRSF_FRAMETYPE_RC_CHANNELS_PACKED, &pkt, sizeof(pkt));
+
+    if (millis() - g_last_stats > 1000 / 10) {
+        telemetry_t telemetry = {0};
+        g_last_stats = millis();
+
+        memcpy(&telemetry.gps, crsf.getGpsSensor(), sizeof(crsf_sensor_gps_t));
+        memcpy(&telemetry.vario, crsf.getVarioSensor(), sizeof(crsf_sensor_vario_t));
+        memcpy(&telemetry.baro, crsf.getBaroAltitudeSensor(), sizeof(crsf_sensor_baro_altitude_t));
+        // memcpy(&telemetry.battery, crsf.getBatterySensor(), sizeof(crsf_sensor_battery_t));
+        esp_now_send(peer_addr, (uint8_t *)&telemetry, sizeof(telemetry));
+    }
 }
 
 void rx_turn_on_led() {
+#if defined(SIMPLE_LED)
     digitalWrite(LED_PIN, LOW);
+#else
+    led_pixel.setPixelColor(0, 0, 255, 0);
+    led_pixel.show();
+#endif
     g_led_on = millis();
 }
-void rx_data_received(const uint8_t *mac, const uint8_t *incomingData, int len) { Serial.printf("Got pkt: %s\n", incomingData); }
+void rx_data_received(const uint8_t *mac, const uint8_t *incomingData, int len) {
+    const rc_data_t *rc_data = (rc_data_t *)incomingData;
+    Serial.printf("Got pkt: %s\n", incomingData);
+    crsf.queuePacket(CRSF_ADDRESS_CRSF_TRANSMITTER, CRSF_FRAMETYPE_RC_CHANNELS_PACKED, &rc_data->channels, len);
+}
 #else
 void rx_setup() {}
 void rx_loop() {}
