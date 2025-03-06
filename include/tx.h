4b67403c8b846238b70041a493a500c9589e7ffe
Dor <dor@amd.com>
2025-03-06 23:22:04 +0200

    wip tx

--- include/tx.h
+++ include/tx.h
@@ -1,6 +1,23 @@
 #pragma once
 #include <Arduino.h>
 
+#define CH_1_PIN 36
+#define CH_2_PIN 39
+#define CH_3_PIN 34
+#define CH_4_PIN 35
+#define CH_5_A_PIN 32
+#define CH_5_B_PIN 33
+#define CH_6_A_PIN 19
+#define CH_6_B_PIN 18
+#define CH_7_UP_PIN 25
+#define CH_7_DOWN_PIN 26
+#define CH_8_UP_PIN 5
+#define CH_8_DOWN_PIN 17
+// add 2 more switches?
+
+#define CH_MIN 0
+#define CH_MAX 4096
+
 void tx_setup();
 void tx_loop();
 void tx_data_received(const uint8_t *mac, const uint8_t *incomingData, int len);
--- src/tx_main.cpp
+++ src/tx_main.cpp
@@ -1,33 +1,96 @@
+#include <Adafruit_SSD1306.h>
 #include <AlfredoCRSF.h>
 #include <Arduino.h>
+#include <RotaryEncoder.h>
 #include <WiFi.h>
+#include <Wire.h>
 #include <esp_now.h>
 #include <esp_wifi.h>
 
 #include "defines.h"
 #include "esp_wifi.h"
+#include "tx.h"
 
 #if defined(WROOM_TX)
 
-static unsigned long currentMillis = 0;
+static unsigned long last_pkt_time = 0;
 static uint8_t peer_addr[] = RX_ADDR;
+static rc_data_t rc_data = {0};
+static RotaryEncoder left_encoder(CH_5_A_PIN, CH_5_B_PIN, RotaryEncoder::LatchMode::TWO03);
+static RotaryEncoder right_encoder(CH_6_A_PIN, CH_6_B_PIN, RotaryEncoder::LatchMode::TWO03);
+static Adafruit_SSD1306 display(128, 32, &Wire, -1);
+// screen layout:
+// mode -----------------------|---------signal-bars|
+// batt -----------------------|-----servo-bars-----|
+// distance & altitude --------|----servo-angles----|
+// speed ----------------------|--------------------|
+// heading --------------------|--------------------|
 
-void tx_setup() { currentMillis = millis(); }
+// splash screen logo
+
+static unsigned get_switch_data(int pinUp, int pinDown) {
+    unsigned ch = 0;
+    if (digitalRead(pinUp) == LOW) {
+        ch = CH_MAX;
+    } else if (digitalRead(pinDown) == LOW) {
+        ch = CH_MIN;
+    } else {
+        ch = CH_MAX / 2;
+    }
+    return ch;
+}
+
+void tx_setup() {
+    pinMode(CH_1_PIN, ANALOG);
+    pinMode(CH_2_PIN, ANALOG);
+    pinMode(CH_3_PIN, ANALOG);
+    pinMode(CH_4_PIN, ANALOG);
+    // range is 0 to 4096
+    left_encoder.setPosition(CH_MAX / 2);
+    right_encoder.setPosition(CH_MAX / 2);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+    }
+    display.clearDisplay();
+    // display.drawBitmap(0, 0, splash_logo, 128, 32, 1);
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+}
 
 void tx_loop() {
-    if (millis() - currentMillis > 1000) {
-        uint8_t myData[] = "ALIVE!\x00";
-        esp_err_t result = esp_now_send(peer_addr, (uint8_t *)&myData, sizeof(myData));
+    left_encoder.tick();
+    right_encoder.tick();
+    // get analog data - map analog read to CH_MIN and CH_MAX
+    rc_data.channels.ch0 = map(analogRead(CH_1_PIN), 0, 4096, CH_MIN, CH_MAX);
+    rc_data.channels.ch1 = map(analogRead(CH_2_PIN), 0, 4096, CH_MIN, CH_MAX);
+    rc_data.channels.ch2 = map(analogRead(CH_3_PIN), 0, 4096, CH_MIN, CH_MAX);
+    rc_data.channels.ch3 = map(analogRead(CH_4_PIN), 0, 4096, CH_MIN, CH_MAX);
+    // cap encoders value between CH_MIN and CH_MAX
+    rc_data.channels.ch4 = constrain(left_encoder.getPosition(), CH_MIN, CH_MAX);
+    rc_data.channels.ch5 = constrain(right_encoder.getPosition(), CH_MIN, CH_MAX);
+    // get switches data
+    rc_data.channels.ch6 = get_switch_data(CH_7_UP_PIN, CH_7_DOWN_PIN);
+    rc_data.channels.ch7 = get_switch_data(CH_8_UP_PIN, CH_8_DOWN_PIN);
 
-        if (result == ESP_OK) {
-        } else {
+    if (millis() - last_pkt_time > 1000 / 100) {
+        esp_err_t result = esp_now_send(peer_addr, (uint8_t *)&rc_data, sizeof(rc_data));
+
+        if (result != ESP_OK) {
             Serial.println("Error sending the data");
         }
-        currentMillis = millis();
+        last_pkt_time = millis();
     }
 }
 
-void tx_data_received(const uint8_t *mac, const uint8_t *incomingData, int len) { Serial.printf("Got pkt: %s\n", incomingData); }
+void tx_data_received(const uint8_t *mac, const uint8_t *incomingData, int len) {
+    Serial.printf("Got pkt: %s\n", incomingData);
+    // parse telemetry data
+    const telemetry_t *telemetry = (telemetry_t *)incomingData;
+    Serial.printf("GPS: %d, %d, %d\n", telemetry->gps.latitude, telemetry->gps.longitude, telemetry->gps.altitude);
+    Serial.printf("Vario: %d\n", telemetry->vario.verticalspd);
+    Serial.printf("Baro: %d\n", telemetry->baro.altitude);
+}
 #else
 void tx_setup() {}
 
